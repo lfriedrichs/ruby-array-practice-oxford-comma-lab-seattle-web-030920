4fa34b956d64ad8d0721b2bd0a6f0a7604c0505e
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-06-28 16:35:05 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length > 2
+    array[-1] = "and #{array.last}"
+  end
+  if array.length == 2 
+    array.join(' and ') 
+  else 
+    array.join(', ') 
+  end
 end
